0ab6ab46c7deb1f2001cfecd2db0dbe706b9c942
felipe Rey <andrespipe.rey@hotmail.com>
2026-05-13 22:02:29 -0500

    mi primer commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using calculadora;
+
+calculadora_met_sobrecarga calculadora = new calculadora_met_sobrecarga();
+
+Console.WriteLine("Multiplicación de 2 números enteros: " + calculadora.calcular(98, 4));
+Console.WriteLine("división de 2 números decimales: " + calculadora.calcular(10.5, 2.5));
+Console.WriteLine("potencia de 2 números enteros: " + calculadora.calcular(3, 4, true));
+Console.WriteLine("potencia de 2 decimales: " + calculadora.calcular(2.5, 3.0, true));
---
+++ calculadora_met_sobrecarga.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calculadora
+{
+    public class calculadora_met_sobrecarga
+    {
+
+        public calculadora_met_sobrecarga()
+        {
+        }
+
+        public int calcular(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int calcular(int a, int b, int c)
+        {
+            return a * b * c;
+        }
+
+
+        public double calcular(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("No se puede dividir por cero.");
+            }
+            return a / b;
+        }
+
+        public double calcular(double a, double b, bool esPotencia)
+        {
+            return Math.Pow(a, b);
+        }
+
+        public int calcular(int baseNum, int exponente, bool esPotencia)
+        {
+            return (int)Math.Pow(baseNum, exponente);
+        }
+
+    }
+}
